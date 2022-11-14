daf3d8e57928c80213f0327de0d671c2c9114e80
siddhant2425 <118192889+siddhant2425@users.noreply.github.com>
2022-11-14 20:21:30 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,163 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "77f305be",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#class"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "9a9c555a",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "21beff35",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c306fe91",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6f256506",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "930bb158",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 27,
+   "id": "16f6fa51",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "class student:\n",
+    "    def __init__(self,roll,name,address):\n",
+    "        self.r=roll\n",
+    "        self.n=name\n",
+    "        self.adr=address\n",
+    "    def show(self):\n",
+    "        print(self.r)\n",
+    "        print(self.n)\n",
+    "        print(self.adr)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 28,
+   "id": "964b67e2",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "s1=student(101,'sid','satara')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 29,
+   "id": "e534fe3c",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "101\n",
+      "sid\n",
+      "satara\n"
+     ]
+    }
+   ],
+   "source": [
+    "s1.show()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 30,
+   "id": "16ce56dd",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "s2=student(111,'suraj','satara')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 31,
+   "id": "b36cb9a5",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "111\n",
+      "suraj\n",
+      "satara\n"
+     ]
+    }
+   ],
+   "source": [
+    "s2.show()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "c436f271",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
